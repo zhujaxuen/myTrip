1f9a5d2be6185be39289408bc01251a666b29200
Andrew Zhu <andrewdzhu@gmail.com>
2026-09-08 19:46:47 -0300

    Atualiza destaque de Nanjing

--- data.js
+++ data.js
@@ -110,7 +110,7 @@ const TRIP = {
       lat: 32.0603,
       lon: 118.7969,
       date: "21–24 out",
-      tag: "História",
+      tag: "Memorial do massacre de Nanjing",
       labelOffset: [0, -0.025, 0.05],
       description:
         "Viagem de trem partindo de Beijing no dia 21/10. Estadia até 24/10.",
